393f31516f9d85c6f732a75ea4151980b08ab61b
Panda0412 <nasb_99@mail.ru>
2023-03-30 16:35:19 +0300

    Fixes

--- ChatApp/DataService/FileService.swift
+++ ChatApp/DataService/FileService.swift
@@ -23,38 +23,39 @@ class FileService {
     }
     
     func saveData(userData: UserProfileViewModel, completion: @escaping (Result<UserProfileViewModel, Error>) -> Void) {
-        readData { [weak self] result in
-            guard let self else { return }
+        var prevData = UserProfileViewModel()
+        readData { result in
             switch result {
                 case .success(let user):
-                    do {
-                        if let avatar = userData.image, avatar != user.image {
-                            if let data = avatar.jpegData(compressionQuality: 1) {
-                                try data.write(to: self.avatarPath)
-                            }
-                        }
-                        if let nickname = userData.nickname, nickname != user.nickname {
-                            try nickname.write(to: self.usernamePath, atomically: true, encoding: .utf8)
-                        }
-                        if let description = userData.description, description != user.description {
-                            try description.write(to: self.descriptionPath, atomically: true, encoding: .utf8)
-                        }
-                        
-                        var savedAvatar: UIImage?
-                        
-                        if let savedJpeg = try? Data(contentsOf: self.avatarPath) {
-                            savedAvatar = UIImage(data: savedJpeg)
-                        }
-                        
-                        let savedUser = UserProfileViewModel(nickname: try String(contentsOf: self.usernamePath, encoding: .utf8), description: try String(contentsOf: self.descriptionPath, encoding: .utf8), image: savedAvatar)
-                        
-                        completion(.success(savedUser))
-                    } catch {
-                        completion(.failure(FileServiceError.writingError))
-                    }
-                case .failure:
-                    completion(.failure(FileServiceError.readingError))
+                    prevData = user
+                case .failure: break
             }
+        }
+        
+        do {
+            if let avatar = userData.image, avatar != prevData.image {
+                if let data = avatar.jpegData(compressionQuality: 1) {
+                    try data.write(to: avatarPath)
+                }
+            }
+            if let nickname = userData.nickname, nickname != prevData.nickname {
+                try nickname.write(to: usernamePath, atomically: true, encoding: .utf8)
+            }
+            if let description = userData.description, description != prevData.description {
+                try description.write(to: descriptionPath, atomically: true, encoding: .utf8)
+            }
+            
+            var savedAvatar: UIImage?
+            
+            if let savedJpeg = try? Data(contentsOf: avatarPath) {
+                savedAvatar = UIImage(data: savedJpeg)
+            }
+            
+            let savedUser = UserProfileViewModel(nickname: try String(contentsOf: usernamePath, encoding: .utf8), description: try String(contentsOf: descriptionPath, encoding: .utf8), image: savedAvatar)
+            
+            completion(.success(savedUser))
+        } catch {
+            completion(.failure(FileServiceError.writingError))
         }
     }
     
@@ -62,11 +63,11 @@ class FileService {
         do {
             var savedAvatar: UIImage?
             
-            if let savedJpeg = try? Data(contentsOf: self.avatarPath) {
+            if let savedJpeg = try? Data(contentsOf: avatarPath) {
                 savedAvatar = UIImage(data: savedJpeg)
             }
             
-            let savedUser = UserProfileViewModel(nickname: try String(contentsOf: self.usernamePath, encoding: .utf8), description: try String(contentsOf: self.descriptionPath, encoding: .utf8), image: savedAvatar)
+            let savedUser = UserProfileViewModel(nickname: try String(contentsOf: usernamePath, encoding: .utf8), description: try String(contentsOf: descriptionPath, encoding: .utf8), image: savedAvatar)
             
             completion(.success(savedUser))
         } catch {
